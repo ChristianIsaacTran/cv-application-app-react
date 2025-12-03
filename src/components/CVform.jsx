5f857575c90b5e25a2bca249e33c1a78498249c7
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-12-02 21:23:36 -0600

    feat: added a final thank you message after submission and fixed inline styling issue
    I imported the finalCV.jsx into the CVform.jsx file
    to re-render the page after submission with a useState,
    but I was having trouble setting an inline
    style with the <main> tag, but then I realized
    that you have to define the style attribute with
    an object with css within the javascript curly
    bracket jsx. Like this: style={{css-style: "thing"}}.
    This let me overwrite the background CSS style and
    fixed the re-rendering issue.

--- src/components/CVform.jsx
+++ src/components/CVform.jsx
@@ -1,9 +1,27 @@
 import GeneralInfo from "./generalInfo.jsx";
 import Education from "./education.jsx";
 import Experience from "./experience.jsx";
+import FinalCV from "./finalCV.jsx";
+import {useState} from "react";
 import "../styles/CVform.css";
 
 function MainForm() {
+    const [finalSubmit, setFinalSubmit] = useState(false);
+
+    const mainSubmitHandler = (e) => {
+        e.preventDefault();
+
+        setFinalSubmit(true);
+    }
+
+    if(finalSubmit === true) {
+        return(
+            <main style={{background: "none"}}>
+                <FinalCV />
+            </main>
+        );
+    }
+
   return (
     <main>
       <h1 className="app-title">CV React App</h1>
@@ -12,7 +30,7 @@ function MainForm() {
         <Education />
         <Experience />
       </div>
-      <button className="main-submit" onClick={(e) => e.preventDefault()}>
+      <button className="main-submit" onClick={mainSubmitHandler}>
         Submit
       </button>
     </main>
--- src/components/finalCV.jsx
+++ src/components/finalCV.jsx
@@ -1,7 +1,7 @@
-
+import "../styles/finalCV.css";
 
 function FinalCV() {
-    return();
+  return <h1 className="thank-you-message">Thank you for your submission!</h1>;
 }
 
 export default FinalCV;
