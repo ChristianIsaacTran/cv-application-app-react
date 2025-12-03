1681e794f7edd8ab5d170ae3e020bf164d3c67c8
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-12-02 19:29:15 -0600

    feat: finished education section with working checkbox useState
    my main issue with working with these new inputs is trying to
    keep the inputs in it's state when toggling between
    submission and editing mode. The checkbox wasn't staying checked
    if the user submitted the checkbox in the checked state, then
    going back and editing the checkbox which would re-render.
    I found out that setting the checked attribute on the
    input tag with a boolean value toggled the checkbox
    state manually, so after making that change, it worked.

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -3,46 +3,116 @@ import { useState } from "react";
 import "../styles/education.css";
 
 function Education() {
+  // education fields info
   const [info, setInfo] = useState({
     schoolName: "",
     major: "",
     startDate: "",
     endDate: "",
-    stillAttending: false,
     editing: true,
   });
 
-  const [userInput, setUserInput] = useState({schoolNameInput:"", majorInput:"", startDateInput:"", endDateInput: "", stillAttendingInput: false});
+  //   input field data
+  const [userInput, setUserInput] = useState({
+    schoolNameInput: "",
+    majorInput: "",
+    startDateInput: "",
+    endDateInput: "",
+  });
 
+  //   checkbox data
+  const [stillAttending, setStillAttending] = useState(false);
+
+  //   upon form submission, re-render the component in submission mode by toggling the info.editing boolean
   const submitHandler = (e) => {
     e.preventDefault();
 
     const tempInfo = {
-      schoolName: e.target.schoolName,
-      major: e.target.major,
-      startDate: e.target.startDate,
-      endDate: e.target.endDate,
-      stillAttending: e.target.attending,
-      editing: true,
+      schoolName: e.target.schoolName.value,
+      major: e.target.major.value,
+      startDate: e.target.startDate.value,
+      endDate: e.target.endDate.value,
+      editing: false,
     };
 
     setInfo(tempInfo);
   };
 
+  //   when going back to edit mode, toggle info.editing to true and trigger re-render
+  const editHandler = () => {
+    const tempInfo = { ...info, editing: true };
+
+    setInfo(tempInfo);
+  };
+
+  //   handle controlled components and their changes within the input fields with the onChange attribute
   const inputChangeHandler = (e) => {
-    if(e.target.id === "school-name") {
-        setUserInput({...userInput, schoolNameInput: e.target.value});
-    } else if(e.target.id === "school-major") {
-        setUserInput({...userInput, majorInput: e.target.value});
-    } else if(e.target.id === "start-date") {
-        setUserInput({...userInput, startDateInput: e.target.value});
-    } else if(e.target.id === "end-date") {
-        setUserInput({...userInput, endDateInput: e.target.value});
-    } else if(e.target.id === "still-attending") {
-        setUserInput({...userInput, stillAttendingInput: e.target.value});
+    if (e.target.id === "school-name") {
+      setUserInput({ ...userInput, schoolNameInput: e.target.value });
+    } else if (e.target.id === "school-major") {
+      setUserInput({ ...userInput, majorInput: e.target.value });
+    } else if (e.target.id === "start-date") {
+      setUserInput({ ...userInput, startDateInput: e.target.value });
+    } else if (e.target.id === "end-date") {
+      setUserInput({ ...userInput, endDateInput: e.target.value });
     }
+  };
+
+  //   handler will set the checkbox boolean depending on the checkbox's state onChange
+  const checkboxHandler = (e) => {
+    setStillAttending(e.target.checked);
+  };
+
+  //  based on the boolean state of the checkbox, it will render yes or no as cosmetic JSX
+  const stillAttendingSubmitRender = () => {
+    if (stillAttending === true) {
+      return <p>Yes</p>;
+    }
+
+    return <p>No</p>;
+  };
+
+  //   if not editing then display submission render
+  if (info.editing === false) {
+    return (
+      <fieldset className="education-container">
+        <legend>Education</legend>
+
+        <div className="school-name-container">
+          <label htmlFor="">School Name:</label>
+          <p>{info.schoolName}</p>
+        </div>
+
+        <div className="major-container">
+          <label htmlFor="">Major: </label>
+          <p>{info.major}</p>
+        </div>
+
+        <div className="start-date-container education">
+          <label htmlFor="">Start Date:</label>
+          <p>{info.startDate}</p>
+        </div>
+
+        <div className="end-date-container education">
+          <label htmlFor="">End Date:</label>
+          <p>{info.endDate}</p>
+        </div>
+
+        <div className="still-attending-container">
+          <label htmlFor="">Still attending?</label>
+          {stillAttendingSubmitRender()}
+        </div>
+
+        <div className="education-button-container">
+          <button className="education-edit" onClick={editHandler}>
+            Edit
+          </button>
+        </div>
+      </fieldset>
+    );
   }
 
+//   main editing render
   return (
     <form action="" className="education-form" onSubmit={submitHandler}>
       <fieldset className="education-container">
@@ -50,32 +120,62 @@ function Education() {
 
         <div className="school-name-container">
           <label htmlFor="">School Name:</label>
-          <input type="text" name="schoolName" id="school-name" value={userInput.schoolNameInput} onChange={inputChangeHandler}/>
+          <input
+            type="text"
+            name="schoolName"
+            id="school-name"
+            value={userInput.schoolNameInput}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div className="major-container">
           <label htmlFor="">Major: </label>
-          <input type="text" name="major" id="school-major" value={userInput.majorInput} onChange={inputChangeHandler} />
+          <input
+            type="text"
+            name="major"
+            id="school-major"
+            value={userInput.majorInput}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div className="start-date-container education">
           <label htmlFor="">Start Date:</label>
-          <input type="date" name="startDate" id="start-date" value={userInput.startDateInput} onChange={inputChangeHandler}/>
+          <input
+            type="date"
+            name="startDate"
+            id="start-date"
+            value={userInput.startDateInput}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div className="end-date-container education">
           <label htmlFor="">End Date:</label>
-          <input type="date" name="endDate" value={userInput.endDateInput} id="end-date" onChange={inputChangeHandler}/>
+          <input
+            type="date"
+            name="endDate"
+            value={userInput.endDateInput}
+            id="end-date"
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div className="still-attending-container">
           <label htmlFor="">Still attending?</label>
-          <input type="checkbox" name="attending" value={userInput.stillAttendingInput} id="still-attending" onChange={inputChangeHandler} />
+
+          <input
+            type="checkbox"
+            name="attending"
+            id="still-attending"
+            onChange={checkboxHandler}
+            checked={stillAttending}
+          />
         </div>
 
         <div className="education-button-container">
           <button className="education-submit">Submit</button>
-          <button className="education-edit">Edit</button>
         </div>
       </fieldset>
     </form>
